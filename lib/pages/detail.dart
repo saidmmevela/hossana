e69059cb32dc225ffb1350497d424d2c13a80ccf
saidmmevella <saidmmevella1@gmail.com>
2022-01-18 17:43:54 +0300

    application structure

---
+++ lib/pages/detail.dart
@@ -0,0 +1,3 @@
+//Author said mmevela
+import 'package:flutter/material.dart';
+//Author said mmevela
---
+++ lib/pages/login.dart
@@ -0,0 +1,20 @@
+//Author said mmevela
+import 'package:flutter/material.dart';
+class Login extends StatefulWidget {
+  const Login({ Key? key }) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Center(
+        child: Text("Login"),
+      ),
+    );
+  }
+}
+//Author said mmevela
